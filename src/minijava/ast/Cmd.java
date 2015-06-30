59917d2714f1e3551860ed9b343efbad0ab31f4f
igorvaz <ivaz567@hotmail.com>
2015-06-30 12:22:04 -0300

    desfazendo cagada

--- src/minijava/ast/Cmd.java
+++
@@ -1,7 +0,0 @@
-package minijava.ast;
-
-import minijava.TabSimb;
-
-public interface Cmd {
-	void tipos(Classe self, TabSimb<Classe> classes, TabSimb<String> vars);
-}
--- src/minijava/ast/Exp.java
+++
@@ -1,7 +0,0 @@
-package minijava.ast;
-
-import minijava.TabSimb;
-
-public interface Exp {
-	String tipo(Classe self, TabSimb<Classe> classes, TabSimb<String> vars);
-}
