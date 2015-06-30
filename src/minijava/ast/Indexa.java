a4a24681d030658146bb1b4d2693af32969d3e5a
igorvaz <ivaz567@hotmail.com>
2015-06-30 11:51:40 -0300

    subindo projeto

---
+++ src/minijava/ast/Indexa.java
@@ -0,0 +1,30 @@
+package minijava.ast;
+
+import minijava.TabSimb;
+
+public class Indexa implements Exp {
+	public Exp vet;
+	public Exp ind;
+	public int lin;
+	
+	public Indexa(Exp _vet, Exp _ind, int _lin) {
+		vet = _vet;
+		ind = _ind;
+		lin = _lin;
+	}
+	
+	public String toString() {
+		return vet + "[" + ind + "]";
+	}
+
+	@Override
+	public String tipo(Classe self, TabSimb<Classe> classes,
+			TabSimb<String> vars) {
+		// TODO: tipagem da expressão de indexação
+		// o tipo de uma indexação é sempre "int", mas antes precisamos:
+		//   verificar que o tipo de "tvet" é compatível com "int[]"
+		//   verificar que o tipo de "tind" é compatível com "int"
+		return null;
+	}
+
+}
