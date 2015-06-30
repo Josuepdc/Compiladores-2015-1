4efbbbf87852569210ea71ad9753a50b06b6288e
Josué Pereira <jospdc@gmail.com>
2015-06-30 13:41:15 -0300

    Adicionando o trabalho 3

---
+++ MiniJavaType/minijava/ast/Campo.java
@@ -0,0 +1,31 @@
+package minijava.ast;
+
+import minijava.TabSimb;
+
+public class Campo implements Exp {
+	public Exp obj;
+	public String nome;
+	public int lin;
+	
+	public Campo(Exp _obj, String _nome, int _lin) {
+		obj = _obj;
+		nome = _nome;
+		lin = _lin;
+	}
+
+	public String toString() {
+		return obj + "." + nome;
+	}
+
+	@Override
+	public String tipo(Classe self, TabSimb<Classe> classes,
+			TabSimb<String> vars) {
+		// TODO: implemente esse método
+		// Para saber o tipo de um acesso a campo:
+		//   obter o tipo da expressão "obj"
+		//   verificar se existe uma classe declarada com esse tipo
+		//   verificar se a classe tem um campo chamado "nome"
+		// Se todas as verificações acima passam, o tipo do acesso é o tipo do campo
+		return null;
+	}
+}
